4e2a0d78e09a5a088ed551a670e278694f00893d
Fantomas42 <fantomas42@gmail.com>
2009-07-16 21:48:50 +0200

    deporting connection, for usage in setup.py

--- bin/testing.py
+++ bin/testing.py
@@ -4,11 +4,8 @@ import unittest
 
 sys.path.append(os.path.abspath('.'))
 
-from veliberator.models import db_connection
-
 from tests import global_test_suite
 
 if __name__ == '__main__':
-    db_connection('sqlite:///:memory:')
     unittest.TextTestRunner(verbosity=2).run(global_test_suite)
     
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,15 @@
 """Global suite of tests"""
 import unittest
 
+from veliberator.models import db_connection
+
 from tests.test_popchecker import suite as popchecker_suite
 from tests.test_models import suite as models_suite
 from tests.test_xml_wrappers import suite as xml_suite
 from tests.test_station import suite as station_suite
 from tests.test_cartography import suite as cartography_suite
+
+db_connection('sqlite:///:memory:')
 
 global_test_suite = unittest.TestSuite([popchecker_suite,
                                         models_suite,
