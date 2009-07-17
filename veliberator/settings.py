08cc0bb71dae86c495d07be96230db2196db2510
Fantomas42 <fantomas42@gmail.com>
2009-07-17 13:53:40 +0200

    cleaning settings

--- veliberator/settings.py
+++ veliberator/settings.py
@@ -1,12 +1,5 @@
 """Settings for Veliberator"""
 
-MAIL_SSL = True
-MAIL_PORT = MAIL_SSL and 995 or 110
-MAIL_SERVER = 'pop.gmail.com'
-MAIL_ADDRESS = 'fantomas42@gmail.com'
-MAIL_ACCOUNT = MAIL_ADDRESS
-MAIL_PASSWORD = '.4il1>Y$'
-
 VELIB_DATA_XML_URL = 'http://www.velib.paris.fr/service/carto'
 VELIB_STATUS_XML_URL = 'http://www.velib.paris.fr/service/stationdetails/%s'
 
