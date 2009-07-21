503c816adc8f14e929f5903228fca8656fac48fe
Fantomas42 <fantomas42@gmail.com>
2009-07-21 17:30:56 +0200

    deporting information acquisition and adding docstrings

--- veliberator/station.py
+++ veliberator/station.py
@@ -10,54 +10,69 @@ class UnknowStation(Exception):
     pass
 
 class Station(object):
-    """Station object"""
+    """Station object,
+    combinating informations and status"""
 
     def __init__(self, velib_id):
         self.id = int(velib_id)
-        try:
-            self.informations = StationInformation.get(self.id)
-        except AttributeError:
-            db_connection()
-            self.informations = StationInformation.get(self.id)
+        self.informations = self.acquire_informations()
 
         if not self.informations:
             raise UnknowStation('The Station ID does not exist.')
 
         self.status = StationStatus(self.id)
 
+    def acquire_informations(self):
+        """Default method for acquiring informations
+        of a stations, by connecting to the database"""
+        try:
+            return StationInformation.get(self.id)
+        except AttributeError:
+            db_connection()
+            return StationInformation.get(self.id)
+
     @property
     def is_open(self):
+        """Property telling if the station is open"""
         return self.informations.opened
 
     @property
     def is_bonus(self):
+        """Property telling if the station is bonus"""
         return self.informations.bonus
 
-    def is_free(self, places=1):        
+    def is_free(self, places=1):
+        """Method property if the station
+        has free places"""
         return self.is_open and self.status.free >= places
 
     def is_available(self, places=1):
+        """Method property if the station
+        has available places"""
         return self.is_open and self.status.available >= places
 
     def __repr__(self):
         if self.informations:
             return '<Station "%s" (%s)>' % (self.id, self.informations.address)
         return '<Station "%s">' % self.id
-        
+
     def compute_distances(self, stations):
+        """Compute the distance of the station,
+        form other stations"""
         distances = {}
 
         for station in stations:
             if station.id == self.id:
                 continue
             l1 = float(self.informations.lat) - float(station.lat)
-            l2 = float(self.informations.lng) - float(station.lng) 
+            l2 = float(self.informations.lng) - float(station.lng)
             dist = sqrt(pow(l1, 2) + pow(l2, 2))
             distances[station] = dist
 
         return distances
-        
+
     def get_stations_around(self, number=5):
+        """Find the stations around"""
         lat_orig = float(self.informations.lat)
         lat_pos = '%.2f' % (lat_orig + 0.01)
         lat_neg = '%.2f' % (lat_orig - 0.01)
@@ -81,7 +96,7 @@ class Station(object):
         station_distances_ordered = sorted(station_distances.iteritems(),
                                            key=lambda (k,v): (v,k))
         stations_id = [station.id for station, distance in station_distances_ordered]
-        
+
         return stations_id[:number]
 
 
