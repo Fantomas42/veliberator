5258ce5a9a8352ba20ba57343cb537385cc0f668
Fantomas42 <fantomas42@gmail.com>
2009-10-27 22:19:40 +0100

    adding lot of features and corrections

--- bin/find_place.py
+++ bin/find_place.py
@@ -2,27 +2,46 @@
 import sys, os
 from optparse import OptionParser
 
+import sqlalchemy
+
 sys.path.append(os.path.abspath('.'))
 
 from veliberator import Station
+from veliberator import VERSION
+from veliberator import Cartography
 from veliberator.models import db_connection
 from veliberator.settings import DATABASE_URI
 from veliberator.geofinder import GeoFinderError
 from veliberator.geofinder import AddressGeoFinder
+from veliberator.models import StationInformation
 
-def show_status(station):
+def check_database_content():
+    if not StationInformation.query.count():
+        print '-> Synchronisation en ligne...'
+        Cartography.synchronize()
+        print '-> Synchronisation complete !'
+
+def show_status(station, distance=None):
     print "Station '%s'" % station.id
-    print station.informations.full_address
+    if distance:
+        print '%s (%.2fm)' % (station.informations.full_address, distance)
+    else:
+        print station.informations.full_address
     print '%s/%s velo(s) disponible' % (station.status.available, station.status.total)
     print '%s place(s) disponible' % station.status.free
 
-def display_free_stations(stations, places):
-    for station_id in stations:
-        station = Station(station_id)
+def display_free_stations(stations, places, max_display):
+    displayed = 0
+
+    for station_information in stations:
+        if displayed == max_display:
+            break
+        station = Station(station_information.id)
         if station.is_free(places):
             print '----------'
-            show_status(station)
-        
+            show_status(station, station_information.distance)
+            displayed += 1
+    
 
 if __name__ == '__main__':   
     parser = OptionParser(usage='usage: %prog [station_id] [options]')
@@ -31,10 +50,19 @@ if __name__ == '__main__':
     parser.add_option('-p', '--places', dest='places', type='int',
                       help='The number of places you want', default=1)
     parser.add_option('-m', '--max_stations', dest='max_stations', type='int',
-                      help='The maximun stations around proposed', default=4)
+                      help='The maximun stations around proposed', default=5)
     (options, args) = parser.parse_args()
-    
-    db_connection(options.database)
+
+    print '-==* Veliberator Find Place v%s *==-' % VERSION
+
+    try:
+        db_connection(options.database)
+    except sqlalchemy.exc.OperationalError:
+        print '-> Database innacessible, switch sur la RAM.'
+        print '-> Modifiez le fichier de configuration, pour enlever ce message.'
+        db_connection('sqlite://')
+
+    check_database_content()
     
     if args:
         user_input = args[0]
@@ -46,12 +74,12 @@ if __name__ == '__main__':
         show_status(station)
         if not station.is_free(options.places):
             print 'Calcul des places les plus proches...'
-            display_free_stations(station.stations_around[:options.max_stations],
-                                  options.places)
+            display_free_stations(station.stations_around,
+                                  options.places, options.max_stations)
     else:
         try:
-            finder = AddressGeoFinder(user_input)
+            finder = AddressGeoFinder(user_input)            
         except GeoFinderError:
             sys.exit('Votre addresse n\'est pas valide ou imprecise.')
-        display_free_stations(finder.get_stations_around()[:options.max_stations],
-                              options.places)
+        display_free_stations(finder.get_stations_around(),
+                              options.places, options.max_stations)
