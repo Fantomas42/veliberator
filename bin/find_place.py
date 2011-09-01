3dffd5838a5aa39c98736bf6860ec042629d2269
Fantomas42 <fantomas42@gmail.com>
2011-09-01 21:52:47 +0200

    removing useless files

--- bin/find_place.py
+++
@@ -1,7 +0,0 @@
-#!/home/fantomas/dev/veliberator/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'veliberator==0.3.dev','find_place.py'
-__requires__ = 'veliberator==0.3.dev'
-from pkg_resources import require; require('veliberator==0.3.dev')
-del require
-__file__ = '/home/fantomas/dev/veliberator/veliberator/scripts/find_place.py'
-execfile(__file__)
