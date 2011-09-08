5396303ca354069c8dabd84a524eee51e6706ea4
Fantomas42 <fantomas42@gmail.com>
2011-09-08 17:02:08 +0200

    better translation

--- veliberator/__init__.py
+++ veliberator/__init__.py
@@ -1,7 +1,4 @@
 """Veliberator module"""
-import os
-import gettext
-
 __version__ = '0.3.dev'
 __license__ = 'BSD License'
 
@@ -9,6 +6,3 @@ __author__ = 'Fantomas42'
 __email__ = 'fantomas42@gmail.com'
 
 __url__ = 'http://veliberator.com/'
-
-gettext.install('veliberator', os.path.join(os.path.dirname(__file__),
-                                            'locale'), unicode=1)
